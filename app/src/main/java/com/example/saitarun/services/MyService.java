80b6b128ab3efa0fb460ca29aecbe52e04e237a3
KANIGANTI SAI TARUN <saitarunkaniganti@gmail.com>
2018-03-25 20:03:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/saitarun/services/MyService.java
@@ -0,0 +1,41 @@
+package com.example.saitarun.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.IBinder;
+
+public class MyService extends Service {
+
+    MediaPlayer mp;
+
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mp=MediaPlayer.create(this,R.raw.abc);
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mp.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mp.stop();
+
+    }
+}
